f733b37433905971c89201a957c9c8216196874d
Elias Velardez <elias.velardez@bigbox.com.ar>
2022-11-01 03:31:53 -0300

    refactor: refactored tests and gitignored hardhat config

--- test/test.ts
+++ test/test.ts
@@ -8,29 +8,16 @@ describe("ETHPool", () => {
 		ethPool: any,
 		accounts: SignerWithAddress[],
 		owner: SignerWithAddress,
-		ownerConnectedEthPool: any,
-		funder: SignerWithAddress,
-		funderConnectedEthPool: any,
-		funderB: SignerWithAddress,
-		connectedEthPoolB: any,
 		teamMember: SignerWithAddress,
-		teamMemberConnectedEthPool: any;
+		funder1: SignerWithAddress,
+		funder2: SignerWithAddress;
 	beforeEach(async () => {
 		ethPoolFactory = await ethers.getContractFactory("ETHPool");
 		ethPool = await ethPoolFactory.deploy();
 		accounts = await ethers.getSigners();
-		owner = accounts[0];
-		ownerConnectedEthPool = await ethPool.connect(owner);
 
-		funder = accounts[1];
-		funderConnectedEthPool = await ethPool.connect(funder);
-
-		funderB = accounts[2];
-		connectedEthPoolB = await ethPool.connect(funderB);
-
-		teamMember = accounts[3];
-		await ownerConnectedEthPool.addTeamMember(teamMember.address);
-		teamMemberConnectedEthPool = await ethPool.connect(teamMember);
+		[owner, teamMember, funder1, funder2] = accounts;
+		await ethPool.connect(owner).addTeamMember(teamMember.address);
 	});
 	const value1 = ethers.utils.parseEther("1");
 	const value100 = ethers.utils.parseEther("100");
@@ -52,33 +39,33 @@ describe("ETHPool", () => {
 	});
 	it("Should fail if the team rewards with the totalAmount of the pool being 0", async () => {
 		await expect(
-			teamMemberConnectedEthPool.reward({ value: value1 })
+			ethPool.connect(teamMember).reward({ value: value1 })
 		).to.be.revertedWithCustomError(ethPool, "EmptyPoolReward");
 	});
 	it("Should update the dividendPerShare when the team gives rewards", async () => {
 		// deposit 1 eth so the totalAmount is > 0 and the reward doesn't fail
-		await funderConnectedEthPool.deposit({ value: value1 });
+		await ethPool.connect(funder1).deposit({ value: value1 });
 
 		// reward 1 eth
-		await teamMemberConnectedEthPool.reward({ value: value1 });
+		await ethPool.connect(teamMember).reward({ value: value1 });
 
 		// the dividendsPerShare should be 1 because we deposited 1 and rewarded 1,
 		// so dividendsPerShare is deposit/previousAmount == 1/1 == 1
 		const magnitude = await ethPool.magnitude();
 		const expectedDividensPerShare = magnitude * 1;
-		const updatedDividensPerShare = await funderConnectedEthPool.dividendsPerShare();
+		const updatedDividensPerShare = await ethPool.dividendsPerShare();
 		assert.equal(expectedDividensPerShare, updatedDividensPerShare);
 	});
 	it("Should update(+) the correction[address] when the address makes a deposit", async () => {
 		// deposit 1 eth so the totalAmount is > 0 and the reward doesn't fail
-		await funderConnectedEthPool.deposit({ value: value1 });
+		await ethPool.connect(funder1).deposit({ value: value1 });
 
 		// we add a reward so the dividendsPerShare is increased from its initial value of 0
-		await teamMemberConnectedEthPool.reward({ value: value1 });
+		await ethPool.connect(teamMember).reward({ value: value1 });
 
 		// then we make a deposit
-		await funderConnectedEthPool.deposit({ value: value1 });
-		const correction = await ethPool.addressToCorrection(funder.address);
+		await ethPool.connect(funder1).deposit({ value: value1 });
+		const correction = await ethPool.addressToCorrection(funder1.address);
 		const dividendsPerShare = await ethPool.dividendsPerShare();
 		const magnitude = await ethPool.magnitude();
 
@@ -87,39 +74,38 @@ describe("ETHPool", () => {
 	});
 	it("Should update(-) the correction[address] when the address makes a withdrawal", async () => {
 		// deposit 1 eth so the totalAmount is > 0 and the reward doesn't fail
-		await funderConnectedEthPool.deposit({ value: value200 });
+		await ethPool.connect(funder1).deposit({ value: value200 });
 
 		// we add a reward so the dividendsPerShare is increased from its initial value of 0
-		await teamMemberConnectedEthPool.reward({ value: value200 });
+		await ethPool.connect(teamMember).reward({ value: value200 });
 
 		// then we make another deposit
-		await funderConnectedEthPool.deposit({ value: value100 });
+		await ethPool.connect(funder1).deposit({ value: value100 });
 
-		// // then we withdraw
-		await funderConnectedEthPool.withdraw();
+		// then we withdraw
+		await ethPool.connect(funder1).withdraw();
 
-		const correction = await ethPool.addressToCorrection(funder.address);
+		const correction = await ethPool.addressToCorrection(funder1.address);
 
 		assert.equal(Number(correction), 0);
 	});
 	it("Should transfer to the address when the address makes a withdrawal", async () => {
-		const funderBalance = await funder.getBalance();
-
+		const funderBalance = await funder2.getBalance();
 		// deposit 1 eth so we can withdraw later
-		const depositTx = await funderConnectedEthPool.deposit({
+		const depositTx = await ethPool.connect(funder2).deposit({
 			value: value100,
 		});
 		const d = await depositTx.wait(1);
 		const depositGasCost = d.gasUsed * d.effectiveGasPrice;
 
 		// withdraw
-		const withdrawTx = await funderConnectedEthPool.withdraw();
+		const withdrawTx = await ethPool.connect(funder2).withdraw();
 		const w = await withdrawTx.wait(1);
 		const withdrawGasCost = w.gasUsed * w.effectiveGasPrice;
 
 		// new balance should be the original - gas used for deposit and withdrawal
 		const expectedBalance = Number(funderBalance) - depositGasCost - withdrawGasCost;
-		const updatedBalance = await funder.getBalance();
+		const updatedBalance = await funder2.getBalance();
 
 		assert.equal(expectedBalance, Number(updatedBalance));
 	});
@@ -129,16 +115,16 @@ describe("ETHPool", () => {
 	// When T deposits 200 rewards, A should be able to withdraw 150 and B 450.
 	it("Situation 1", async () => {
 		// A deposits 100
-		await funderConnectedEthPool.deposit({ value: value100 });
+		await ethPool.connect(funder1).deposit({ value: value100 });
 
 		// B deposits 300
-		await connectedEthPoolB.deposit({ value: value300 });
+		await ethPool.connect(funder2).deposit({ value: value300 });
 
 		// T rewards 200
-		await teamMemberConnectedEthPool.reward({ value: value200 });
+		await ethPool.connect(teamMember).reward({ value: value200 });
 
-		const withdrawableAmountA = await ethPool.withdrawableAmount(funder.address);
-		const withdrawableAmountB = await ethPool.withdrawableAmount(funderB.address);
+		const withdrawableAmountA = await ethPool.withdrawableAmount(funder1.address);
+		const withdrawableAmountB = await ethPool.withdrawableAmount(funder2.address);
 
 		assert.equal(withdrawableAmountA.toString(), value150.toString());
 		assert.equal(withdrawableAmountB.toString(), value450.toString());
@@ -154,16 +140,16 @@ describe("ETHPool", () => {
 	// B should only get their deposit because rewards were sent to the pool before they participated.
 	it("Situation 2", async () => {
 		// A deposits 100
-		await funderConnectedEthPool.deposit({ value: value100 });
+		await ethPool.connect(funder1).deposit({ value: value100 });
 
 		// T rewards 200
-		await teamMemberConnectedEthPool.reward({ value: value200 });
+		await ethPool.connect(teamMember).reward({ value: value200 });
 
 		// B deposits 200
-		await connectedEthPoolB.deposit({ value: value200 });
+		await ethPool.connect(funder2).deposit({ value: value200 });
 
-		const withdrawableAmountA = await ethPool.withdrawableAmount(funder.address);
-		const withdrawableAmountB = await ethPool.withdrawableAmount(funderB.address);
+		const withdrawableAmountA = await ethPool.withdrawableAmount(funder1.address);
+		const withdrawableAmountB = await ethPool.withdrawableAmount(funder2.address);
 
 		// A gets 300 because they deposited 100 and T rewarded 200
 		// BEFORE B deposited 200, so B only gets 200.
