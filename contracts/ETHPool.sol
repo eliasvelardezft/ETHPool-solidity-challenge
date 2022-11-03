b3a94d288d73d51f67bff5baec7dec1cc9aff45d
Elias Velardez <elias.velardez@bigbox.com.ar>
2022-11-03 01:14:13 -0300

    refactor: small details in the contract

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -4,26 +4,25 @@ pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-error UnsuccessfulTransfer();
-error EmptyPoolReward();
+error ETHPool__UnsuccessfulTransfer();
+error ETHPool__EmptyPoolReward();
 
 contract ETHPool is AccessControl {
 	event Deposit(address indexed sender, uint256 amount);
 	event Reward(uint256 amount);
 	event Withdrawal(address indexed sender, uint256 amount);
 
-	bytes32 public constant TEAM_MEMBER = keccak256("TEAM_MEMBER");
-
-	constructor() {
-		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-		_setupRole(TEAM_MEMBER, msg.sender);
-	}
-
+	bytes32 public constant TEAM_MEMBER_ROLE = keccak256("TEAM_MEMBER_ROLE");
 	mapping(address => uint256) public addressToAmountFunded;
 	mapping(address => uint256) public addressToCorrection;
 	uint256 public dividendsPerShare;
 	uint256 public magnitude = 1e18;
 	uint256 public totalAmount;
+
+	constructor() {
+		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+		_setupRole(TEAM_MEMBER_ROLE, msg.sender);
+	}
 
 	function deposit() external payable {
 		totalAmount += msg.value;
@@ -34,8 +33,8 @@ contract ETHPool is AccessControl {
 		emit Deposit(msg.sender, msg.value);
 	}
 
-	function reward() external payable onlyRole(TEAM_MEMBER) {
-		if (!(totalAmount > 0)) revert EmptyPoolReward();
+	function reward() external payable onlyRole(TEAM_MEMBER_ROLE) {
+		if (!(totalAmount > 0)) revert ETHPool__EmptyPoolReward();
 
 		dividendsPerShare += (msg.value * magnitude) / totalAmount;
 		totalAmount += msg.value;
@@ -48,30 +47,32 @@ contract ETHPool is AccessControl {
 
 		// empty sender's amountFunded BEFORE the transfer
 		// to avoid reentrancy attack
-		// (actually because we're using transfer this isn't a problem because of the 2300 gas limit)
+		// (only with call, with transfer this isn't a problem because of the 2300 gas limit)
 		addressToAmountFunded[msg.sender] = 0;
 		addressToCorrection[msg.sender] = 0;
 		totalAmount -= correctedWithdrawableAmount;
 
 		// (bool success, ) = msg.sender.call{ value: correctedWithdrawableAmount }("");
-		// if (!success) revert UnsuccessfulTransfer();
+		// if (!success) revert ETHPool__UnsuccessfulTransfer();
 		payable(msg.sender).transfer(correctedWithdrawableAmount);
 
 		emit Withdrawal(msg.sender, correctedWithdrawableAmount);
 	}
 
 	function withdrawableAmount(address sender) public view returns (uint256) {
-		uint256 uncorrectedAmount = (dividendsPerShare * addressToAmountFunded[sender]) / magnitude;
-		uint256 correctedAmount = uncorrectedAmount - addressToCorrection[sender];
-		return correctedAmount + addressToAmountFunded[sender];
+		uint256 uncorrectedPortionOfDividends = (dividendsPerShare *
+			addressToAmountFunded[sender]) / magnitude;
+		uint256 correctedPortionOfDividends = uncorrectedPortionOfDividends -
+			addressToCorrection[sender];
+		return correctedPortionOfDividends + addressToAmountFunded[sender];
 	}
 
 	// administration
 	function addTeamMember(address _newTeamMember) external {
-		grantRole(TEAM_MEMBER, _newTeamMember);
+		grantRole(TEAM_MEMBER_ROLE, _newTeamMember);
 	}
 
 	function removeTeamMember(address _teamMember) external {
-		revokeRole(TEAM_MEMBER, _teamMember);
+		revokeRole(TEAM_MEMBER_ROLE, _teamMember);
 	}
 }
